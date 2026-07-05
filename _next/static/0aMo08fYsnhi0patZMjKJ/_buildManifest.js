e4cb1d24e5fa9575c3ff15ed9f4396deee6b32bc
Aaroophan <arophn@gmail.com>
2026-07-05 20:09:33 +0530

    Updates

--- _next/static/0aMo08fYsnhi0patZMjKJ/_buildManifest.js
+++ _next/static/0aMo08fYsnhi0patZMjKJ/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/MarkdownLab//_next/:path+",
-        "destination": "/MarkdownLab/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
