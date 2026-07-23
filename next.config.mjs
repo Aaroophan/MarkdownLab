3037b1521bb36b17bea24bdd50a04edc42f9748d
Aaroophan <arophn@gmail.com>
2026-07-23 23:06:58 +0530

    fix: update basePath in next.config.mjs to root for correct routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/MarkdownLab',
+  basePath: '/',
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
